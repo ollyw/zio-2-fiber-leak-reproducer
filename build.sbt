c1f7ecb8174d5bf8a5621e1e9d693078b1b370b2
Oliver Wickham <mr.o.wickham@gmail.com>
2023-01-04 09:17:42 +0000

    ZIO 1.0.15 Experimentt

--- build.sbt
+++ build.sbt
@@ -7,9 +7,9 @@ lazy val root = (project in file("."))
   .settings(
     name := "zio-2-fiber-leak-recreator",
     libraryDependencies ++= Seq(
-      "dev.zio" %% "zio" % "2.0.5",
-      "dev.zio" %% "zio-interop-cats" % "23.0.0.0",
-      "dev.zio" %% "zio-test" % "2.0.5" % Test,
+      "dev.zio" %% "zio" % "1.0.15",
+      "dev.zio" %% "zio-interop-cats" % "13.0.0.1",
+      "dev.zio" %% "zio-test" % "1.0.15" % Test,
       "org.http4s" %% "http4s-blaze-client" % "0.23.12"
     ),
     testFrameworks += new TestFramework("zio.test.sbt.ZTestFramework")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,42 +3,42 @@ import org.http4s.client.Client
 import zio.Fiber.Status
 import zio._
 import zio.interop.catz._
+import zio.interop.catz.implicits._
 
-object Main extends ZIOAppDefault {
-  override def run: ZIO[Environment with ZIOAppArgs with Scope, Any, Any] = fiberMetricsWorker *> poller.provide(httpClient)
+object Main extends App {
+  override def run(args: List[String]) = fiberMetricsWorker *> poller.provideCustomLayer(httpClient).orDie.exitCode
 
   private def poller =
     (for {
       client <- ZIO.service[Client[Task]]
-      result <- client.expect[String]("https://zio.dev/")
+      result <- client.expect[String]("https://zio.dev")
 //      _ = println(result)
-    } yield ()).repeat(Schedule.fixed(2.seconds))
+    } yield ()).repeat(Schedule.forever)
 
-  private def fiberMetricsWorker =
-    (for {
-      roots <- Fiber.roots
-      total = roots.size
-      statues <- ZIO.foldLeft(roots)((0, 0, 0)) { case ((running, suspended, done), fiber) =>
-        fiber.status.map {
-          case _: Status.Running => (running + 1, suspended, done)
-          case _: Status.Suspended => (running, suspended + 1, done)
-          case Status.Done => (running, suspended, done + 1)
-        }
-      }
-      _ <-
-        ZIO.debug(
-          s"Total roots $total. Fibers running: ${statues._1}, suspended: ${statues._2}, done: ${statues._3}"
-        )
-//            sampledRoots = roots.take(20)
-//            _ <- ZIO.foreachDiscard(sampledRoots)(f =>
-//                   f.dump.flatMap(dump => ZIO.when(dump.status.isSuspended)(dump.prettyPrint.flatMap(ZIO.debug(_))))
-//                 )
-    } yield ()).repeat(Schedule.fixed(5.seconds)).fork
+  private def fiberMetricsWorker = ZIO.unit
+//    (for {
+//      roots <- Fiber.roots
+//      total = roots.size
+//      statues <- ZIO.foldLeft(roots)((0, 0, 0)) { case ((running, suspended, done), fiber) =>
+//        fiber.status.map {
+//          case _: Status.Running => (running + 1, suspended, done)
+//          case _: Status.Suspended => (running, suspended + 1, done)
+//          case Status.Done => (running, suspended, done + 1)
+//        }
+//      }
+//      _ <-
+//        ZIO.debug(
+//          s"Total roots $total. Fibers running: ${statues._1}, suspended: ${statues._2}, done: ${statues._3}"
+//        )
+////            sampledRoots = roots.take(20)
+////            _ <- ZIO.foreachDiscard(sampledRoots)(f =>
+////                   f.dump.flatMap(dump => ZIO.when(dump.status.isSuspended)(dump.prettyPrint.flatMap(ZIO.debug(_))))
+////                 )
+//    } yield ()).repeat(Schedule.fixed(5.seconds)).fork
 
 
-  private val httpClient: TaskLayer[Client[Task]] = ZLayer.scoped(
-    ZIO.runtime[Any].flatMap { implicit rts =>
-      BlazeClientBuilder[Task].resource.toScopedZIO
-    }
-  )
+  private val httpClient =
+    ZManaged.runtime[ZEnv].flatMap { implicit rts =>
+      BlazeClientBuilder[Task].resource.toManagedZIO
+    }.toLayer
 }
